1abe2e3fe3e52be033e84b15acdd301e6efe37a0
MolchanovaOlga <designer.molchanova@gmail.com>
2023-11-02 09:32:50 +0200

    Solutions to firstsecond and third tasks are written

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,18 @@
 `use strict`;
 
+function filterArray(numbers, value) {
+    let array = [];
+    for  (let i = 0; i < numbers.length; i += 1) {
+        if (numbers[i] > value) {
+            array.push(numbers[i]);
+        }
+    }
+    return array;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
